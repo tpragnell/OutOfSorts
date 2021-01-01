de341adda95386b7fe2fa907e8cae56af04a65a2
Tristan Pragnell <tristan@Tristans-MBP.fios-router.home>
2021-01-01 12:45:25 -0500

    fixes to bubble sort

--- Sorts.java
+++ Sorts.java
@@ -4,15 +4,44 @@ public class Sorts{
   param data  the elements to be sorted.
   */
   public static void bubbleSort(int[] _data){
-    for(int i=0; i<_data.length; i++){
-      for(int k=i+1; k<_data.length; k++){
-        if(_data[i]>_data[k]){
-          int t = _data[i];
-          _data[i] = _data[k];
-          _data[k] = t;
+    boolean sorted = false;
+    while(!sorted){
+      sorted = true;
+      for(int i = 0; i<_data.length-1; i++){
+        if(_data[i] > _data[i+1]){
+          int temp = _data[i];
+          _data[i] = _data[i+1];
+          _data[i+1] = temp;
+          sorted = false;
         }
       }
     }
   }
 
+  /**selection sort of an int array.
+  *@postcondition The array will be modified such that the elements will be in increasing order.
+  *param data - the elements to be sorted.
+  */
+   public static void selectionSort(int[] data){
+
+   }
+
+  /**insertion sort of an int array.
+  *@postcondition The array will be modified such that the elements will be in increasing order.
+  *param data - the elements to be sorted.
+  */
+   public static void insertionSort(int[] data){
+
+   }
+
 }
+
+//for(int i=0; i<_data.length; i++){
+//   for(int k=i+1; k<_data.length; k++){
+//     if(_data[i]>_data[k]){
+//       int t = _data[i];
+//       _data[i] = _data[k];
+//       _data[k] = t;
+//     }
+//   }
+// }
